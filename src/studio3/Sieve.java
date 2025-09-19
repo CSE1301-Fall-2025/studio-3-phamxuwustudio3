8bc7594632cfc86a137fafff314ff5e121144685
Paul <p.h.pham@wustl.edu>
2025-09-19 11:34:41 -0500

    studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+public class Sieve {
+public static void main (String [] args) { 
+}
+    Scanner in = new Scanner (System.in); {
+        System.out.print ("What number do you want to check?"); 
+         int p = in.nextInt (); 
+          int [] numbers = new int [p]; 
+        int value = 2;
+
+for (int i =0; i<40; i++ ) {
+numbers [i] = value; 
+value++;
+ 
+}
+ for (int m=2; p<40; p=+2) {
+    numbers [i]; 
+ }
+}
+}
